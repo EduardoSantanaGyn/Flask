313ff428e2bb543985c807f60cf1a224d8b7f016
EduardoSantanaGyn <santanaeduardogyn@gmail.com>
2025-03-23 11:49:47 -0300

    salvando2

--- estudo/views.py
+++ estudo/views.py
@@ -1,5 +1,6 @@
-from estudo import app
-from flask import render_template, url_for
+from estudo import app, db
+from flask import render_template, url_for, request
+from estudo.models import Contato
 
 @app.route('/')
 def homepage():
@@ -13,6 +14,28 @@ def homepage():
     }
     return render_template('index.html', context=context)
 
-@app.route('/nova/')
+@app.route('/contato/', methods=['GET','POST'])
 def novapagi():
-    return 'Outra View'
+    context = {}
+    if request.method == 'GET':
+        pesquisa = request.args.get('pesquisa')
+        print('GET: ', pesquisa)
+        context.update({'pesquisa': pesquisa})
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        assunto = request.form['assunto']
+        mensagem = request.form['mensagem']
+
+        contato = Contato(
+            nome=nome,
+            email=email,
+            assunto=assunto,
+            mensagem=mensagem,
+        )
+        
+        db.session.add(contato)
+        db.session.commit()
+
+
+    return render_template('contato.html', context=context)
