702987efa748777483aa4858c4d1ecf9df0a597d
EduardoSantanaGyn <santanaeduardogyn@gmail.com>
2025-03-23 11:49:29 -0300

    Salvando

--- estudo/views.py
+++ estudo/views.py
@@ -1,6 +1,18 @@
 from estudo import app
-from flask import render_template
+from flask import render_template, url_for
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    usuario = 'Duduznx'
+    idade = 19
+
+    context = {
+        'usuario' : usuario,
+        'idade' : idade
+
+    }
+    return render_template('index.html', context=context)
+
+@app.route('/nova/')
+def novapagi():
+    return 'Outra View'
